96e7496bec6a135683d14f897cb691885af4fe62
izayl <izayl@163.com>
2022-07-24 01:59:21 +0800

    feat: add eventsource client

--- src/v2/client.ts
+++ src/v2/client.ts
@@ -1,7 +1,18 @@
-import { BaseClient } from '../client'
+import { BaseClient, Config } from '../client'
 import { Apps, Balances, Exchange, Misc, NFT, Transactions, ZapIn, ZapOut } from './methods'
 
+const DefaultV2ClientConfig: Partial<Config> = {
+  apiHost: 'https://api.zapper.fi',
+}
+
 export class V2Client extends BaseClient {
+  constructor(protected readonly config: Config) {
+    super({
+      ...DefaultV2ClientConfig,
+      ...config,
+    })
+  }
+
   apps = new Apps(this)
   transactions = new Transactions(this)
   misc = new Misc(this)
